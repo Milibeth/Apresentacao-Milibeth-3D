275df9dd7865155286898038e27c1b55d9ca72c4
Milibeth <124629620+Milibeth@users.noreply.github.com>
2024-08-29 01:27:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const primeiroNome = "Milibeth";
+const sobrenome = "Garcia";
+const idade = 17;
+const altura = 1.59;
+const cidadeNatal = "Cumana";
+const massa = 53;
+
+console.log("Meu nome é:")
+console.log(primeiroNome );
+console.log("O meu sobrenome é:")
+console.log(sobrenome );
+console.log("Minha idade:")
+console.log(idade );
+console.log("Minha altura é:")
+console.log(altura);
+console.log("Nasci na cidade de:")
+console.log(cidadeNatal);
+console.log("Meu peso é de:")
+console.log(massa );
